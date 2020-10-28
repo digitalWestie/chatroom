6e210cbb31379e73d66a1451107c708732ebe4e8
R Gianni <hello@rgianni.cc>
2020-10-28 23:33:20 +0000

    restyle ui

--- upbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yarn build
-mv dist chatroomdist
-zip -r chatroom.zip chatroomdist/
-rm  -r ./chatroomdist
-ssh -t root@178.62.43.240 'rm -r /home/digitalwestie/chatroomdist'
-scp chatroom.zip root@178.62.43.240:/home/digitalwestie/
